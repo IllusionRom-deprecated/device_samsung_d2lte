ad0e0c8b6f24ceaef05ae0be4a3fcbc6e08b5ebe
Hieu Phan <aznrice2k4@gmail.com>
2014-03-26 00:14:02 -0500

    ill.mk: we're not cm
    Change-Id: Ie7ffb71128ca9ad3901e79af42b0986f3c7143c1
    Signed-off-by: Hieu Phan <aznrice2k4@gmail.com>

--- ill.mk
+++ ill.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
 # Enhanced NFC
 #$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common illusion stuff.
 $(call inherit-product, vendor/illusion/config/common_full_phone.mk)
 
 PRODUCT_NAME := ill_d2lte
