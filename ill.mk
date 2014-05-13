168b215299a0c3f30a0680c3d6553b5f353c4531
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2014-05-12 23:36:22 -0100

    Change for new bootanimations!
    Change-Id: Ia85a5d3e9c32bc2e86839a430eb226f15427fa6a

--- ill.mk
+++ ill.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/ill/config/nfc_enhanced.mk)
 
 # Inherit some common illusion stuff.
 $(call inherit-product, vendor/illusion/config/common_full_phone.mk)
@@ -10,4 +10,4 @@ PRODUCT_NAME := ill_d2lte
 
 # bootanimation
 PRODUCT_COPY_FILES += \
-        vendor/illusion/bootanimation/Illusion768x768.zip:system/media/bootanimation.zip
+        vendor/illusion/bootanimation/768x1280.zip:system/media/bootanimation.zip
